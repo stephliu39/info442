7d036e193f777347b0ca54a0a3f41f58777f84c2
cammip <cammip@uw.edu>
2024-05-09 16:38:11 -0700

    added js file

---
+++ public/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function(){
+  window.addEventListener("load", init);
+
+  /**
+   * This function runs when the window loads, we can use this to add
+   * even listeners and create new functions under this function
+   */
+  function init() {
+    console.log("hiiii");
+  }
+})();
