a4c5ab9a6a4b643d216c8b18de3a41d861a67a63
cammip <cammip@uw.edu>
2024-05-09 18:09:35 -0700

    progress on login page

--- public/index.js
+++ public/index.js
@@ -9,5 +9,20 @@
    */
   function init() {
     console.log("hiiii");
+    let loginPage = document.querySelector("#login");
+    let signUpPage = document.querySelector("#sign-up");
+
+    // When the user clicks to sign up, 
+    document.getElementById("click-to-sign-up").addEventListener("click", () => {
+      loginPage.classList.add("hidden");
+      signUpPage.classList.remove("hidden");
+    });
+
+    // When the user clicks the back button, the login page shows again
+    let backBtn = document.getElementById("back-btn");
+    backBtn.addEventListener("click", () => {
+      loginPage.classList.remove("hidden");
+      signUpPage.classList.add("hidden");
+    });
   }
 })();
