bde6ebaaed9967c862f5291b3ffbce05d8dfd994
*KG0797* <*jguo25@uw.edu*>
2024-05-15 20:04:49 -0700

    nav to profile

--- public/index.js
+++ public/index.js
@@ -25,4 +25,13 @@
       signUpPage.classList.add("hidden");
     });
   }
+  document.getElementById('nav-profile').addEventListener('click', function() {
+    showSection('profile-page');
+});
+function showSection(sectionId) {
+    document.querySelectorAll('main > section').forEach(section => {
+        section.classList.add('hidden');
+    });
+    document.getElementById(sectionId).classList.remove('hidden');
+}
 })();
